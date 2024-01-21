7a2c6f63aaa9447513919ee83edde4ce09cbc784
rockeywerewolf <mi71k9d7p@code.edu.az>
2024-01-21 14:43:24 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ad = "Nermin";
+            string result = PrintName(ad);
+            Console.WriteLine(result);
+        }
+        static void PrintFullName(string name)
+        {
+            Console.WriteLine($"Your name is {name}");
+        }
+        static string PrintName(string name)
+        {
+            return $"Your name is {name}";
+        }
+    }
+}
